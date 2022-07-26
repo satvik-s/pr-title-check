f09d3d5b687ffff594742e21ea65bd95b7fd820b
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-07-26 16:13:54 -0700

    update code, test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 function run(): void {
     try {
-        const ms: string = core.getInput('milliseconds');
-        core.debug(`Waiting ${ms} milliseconds ...`);
+        const { eventName } = github.context;
+        core.info(`Event name: ${eventName}`);
 
-        core.debug(new Date().toTimeString());
-        core.debug(new Date().toTimeString());
+        if (eventName !== 'pull_request') {
+            core.setFailed(`Invalid event: ${eventName}`);
+            return;
+        }
+
+        const pullRequestTitle = github.context.payload.pull_request?.title;
+        core.info(`PR title: ${pullRequestTitle}`);
+
+        if (!pullRequestTitle) {
+            core.setFailed('Pull Request title not defined');
+            return;
+        }
+
+        const inputRegexString = core.getInput('regex');
+        core.info(`Input regex: ${inputRegexString}`);
+
+        const regex = new RegExp(inputRegexString, 'gm');
+
+        const patternExistsInInput = regex.test(pullRequestTitle);
+
+        if (!patternExistsInInput) {
+            core.setFailed('PR title does not contain regex pattern');
+            return;
+        }
+
+        core.info('Pattern exists in PR title');
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);

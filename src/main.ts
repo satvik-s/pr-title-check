7cae2a237f2ea8f8fbf5a63b16965c6bc22c9703
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-07-26 21:43:02 -0700

    feat: update code, test (#11)
    * update code, test
    
    * update test gha
    
    * nit
    
    * update action
    
    * update code
    
    * debug log
    
    * nit
    
    * fix
    
    * remove debug lo
    
    * add debug log
    
    * nit
    
    * nit2
    
    * try another way
    
    * try another way
    
    * finally
    
    * update
    
    * add release-it changes
    
    * chore: release v0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,49 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const DEFAULT_FLAGS = 'gm';
 
 function run(): void {
     try {
-        const ms: string = core.getInput('milliseconds');
-        core.debug(`Waiting ${ms} milliseconds ...`);
+        const { eventName } = github.context;
+        core.info(`Event name: ${eventName}`);
 
-        core.debug(new Date().toTimeString());
-        core.debug(new Date().toTimeString());
+        if (eventName !== 'pull_request') {
+            core.setFailed(`Invalid event: ${eventName}`);
+            return;
+        }
+
+        const pullRequestTitle: string | undefined =
+            github.context.payload.pull_request?.title;
+        core.info(`PR title: ${pullRequestTitle}`);
+
+        if (!pullRequestTitle) {
+            core.setFailed('Pull Request title not defined');
+            return;
+        }
+
+        const inputPattern = core.getInput('pattern', { required: true });
+        const inputFlags = core.getInput('flags');
+
+        if (inputFlags === '') {
+            core.info('No input flags present. Will fallback to default');
+        }
+
+        const regexPattern = inputPattern;
+        const regexFlags = inputFlags === '' ? DEFAULT_FLAGS : inputFlags;
+
+        core.info(`Pattern: ${regexPattern}`);
+        core.info(`Flags: ${regexFlags}`);
+
+        const regex = new RegExp(regexPattern, regexFlags);
+        const regexExistsInTitle = regex.test(pullRequestTitle);
+
+        if (!regexExistsInTitle) {
+            core.setFailed('PR title does not contain the regex pattern');
+            return;
+        }
+
+        core.info('Pattern exists in PR title');
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
